059ab236fdd0d0e60dae85063c7eea95854a69b5
Sergio <sergiof_2001@hotmail.com>
2022-09-23 18:37:03 -0700

    Doing a code problem

--- app.js
+++ app.js
@@ -27,3 +27,36 @@ let isOpen = false
 let strs = cash >= price && !isOpen ? `Thanks heres $${cash - price}` : 'Not enough'
 
 console.log(strs)
+
+
+// DRY Don't Repeat yourself 
+
+// While Loops
+
+// let count =  1
+// while(count <= 3)
+// {
+//     console.log(count)
+//     count = count + 1
+// }
+
+// for Loop
+// for( let i = 0; i < 3; i++) {
+//     console.log(i)
+// }
+
+for(let i = 1; i <= 20; i++)
+{
+    if (i % 3 === 0 && i % 5 === 0){
+        console.log(`${i} --> Fontend Simplified`)
+    }
+    else if(i % 3 === 0){
+        console.log(`${i} --> Fontend`)
+    }
+    else if (i % 5 === 0){
+        console.log(`${i} --> Simplified`)
+    }
+    else{
+        console.log(`${i} --> ${i}`)
+    }
+}
