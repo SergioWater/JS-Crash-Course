7184f93e24943fd534953d41e988edc31b23dd90
Sergio <sergiof_2001@hotmail.com>
2022-09-23 19:00:16 -0700

    fuinctions

--- app.js
+++ app.js
@@ -1,67 +1,74 @@
-let cash = 50
-let price = 40
+// let cash = 50
+// let price = 40
 
-if (cash > price){
-    console.log(`you paid an extra ${cash - price} dollar`)
-}
-else if (cash === price){
-    console.log(`clean`)
-}
-else{
-    console.log(`not enough pay $${price - cash}`)
-}
-
-let hot = ""
-
-hot ? console.log("Hot outside") : console.log("Cold outside")
-
-let subscribed = false
-let loggIn = true
-
-let str = subscribed || loggIn ? 'Show video' : 'Dont show video'
-
-console.log(str)
-
-let isOpen = false
-
-let strs = cash >= price && !isOpen ? `Thanks heres $${cash - price}` : 'Not enough'
-
-console.log(strs)
-
-
-// DRY Don't Repeat yourself 
-
-// While Loops
-
-// let count =  1
-// while(count <= 3)
-// {
-//     console.log(count)
-//     count = count + 1
+// if (cash > price){
+//     console.log(`you paid an extra ${cash - price} dollar`)
+// }
+// else if (cash === price){
+//     console.log(`clean`)
+// }
+// else{
+//     console.log(`not enough pay $${price - cash}`)
 // }
 
-// for Loop
-// for( let i = 0; i < 3; i++) {
-//     console.log(i)
-// }
+// let hot = ""
 
-// for(let i = 1; i <= 20; i++)
-// {
-//     if (i % 3 === 0 && i % 5 === 0){
-//         console.log(`${i} --> Fontend Simplified`)
-//     }
-//     else if(i % 3 === 0){
-//         console.log(`${i} --> Fontend`)
-//     }
-//     else if (i % 5 === 0){
-//         console.log(`${i} --> Simplified`)
-//     }
-//     else{
-//         console.log(`${i} --> ${i}`)
-//     }
-// }
+// hot ? console.log("Hot outside") : console.log("Cold outside")
 
-let front = "Fontend Simplified"
-for(let i = 0; i < front.length; i++){
-    console.log(front[i])
+// let subscribed = false
+// let loggIn = true
+
+// let str = subscribed || loggIn ? 'Show video' : 'Dont show video'
+
+// console.log(str)
+
+// let isOpen = false
+
+// let strs = cash >= price && !isOpen ? `Thanks heres $${cash - price}` : 'Not enough'
+
+// console.log(strs)
+
+
+// // DRY Don't Repeat yourself 
+
+// // While Loops
+
+// // let count =  1
+// // while(count <= 3)
+// // {
+// //     console.log(count)
+// //     count = count + 1
+// // }
+
+// // for Loop
+// // for( let i = 0; i < 3; i++) {
+// //     console.log(i)
+// // }
+
+// // for(let i = 1; i <= 20; i++)
+// // {
+// //     if (i % 3 === 0 && i % 5 === 0){
+// //         console.log(`${i} --> Fontend Simplified`)
+// //     }
+// //     else if(i % 3 === 0){
+// //         console.log(`${i} --> Fontend`)
+// //     }
+// //     else if (i % 5 === 0){
+// //         console.log(`${i} --> Simplified`)
+// //     }
+// //     else{
+// //         console.log(`${i} --> ${i}`)
+// //     }
+// // }
+
+// // let front = "Fontend Simplified"
+// // for(let i = 0; i < front.length; i++){
+// //     console.log(front[i])
+// // }
+
+function convert(C){
+    return C * 1.8 + 32
 }
+console.log(convert(0))
+console.log(convert(10))
+console.log(convert(30))
